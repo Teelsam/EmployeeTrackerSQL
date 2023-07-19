4f31fb28be82c1fb4433883e8d83b20a175e6737
Sam Teel <sjkteel@hotmail.com>
2023-07-19 12:14:16 -0400

    wrote prompt question

--- index.js
+++ index.js
@@ -1,23 +1,51 @@
 const express = require('express');
+const inquirer = require('inquirer');
 const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 const db = mysql.createConnection(
     {
         host: 'localhost',
-        user: 'root',
-        password: '224Ardmore',
-        database: 'buisness_db'
+        user: 'root', // generic user
+        password: '224Ardmore', //generic pass
+        database: 'buisness_db'//our database
     },
     console.log(`Connected to the buisness_db database.`)
 );
+console.log(`
+-------------------------------
+|~WELCOME TO EMPLOYEE MANAGER~|
+-------------------------------`)
+    ; function runPrompt() {
+        inquirer
+            .prompt([{
+                type: 'list',
+                name: "option",
+                message: "What would you like to do?",
+                choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department']
+            }])
+            .then(data => {
+                console.log('data is: ', data.option);
 
-db.query('SELECT * FROM department', function (err, res) {
-    console.log(res);
-});
+            })
 
+    }
+runPrompt();
+// db.query('SELECT * FROM department', function (err, res, fields) {
+//     console.log(res);
+//     console.log("error from department: ", err);
+// });
+// db.query('SELECT * FROM role', function (err, res) {
+//     console.log(res);
+//     console.log("error from role: ", err);
+// });
+// db.query('SELECT * FROM employee', function (err, res) {
+//     console.log(res);
+//     console.log("error from employee: ", err);
+// });
+// app.use((req, res) => { res.status(404).end(); });
 app.listen(PORT, () => { console.log(`Working on ${PORT}`); });

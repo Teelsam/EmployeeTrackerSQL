5809e0494a897aaf004dc1747f7c89f3badc9c51
Sam Teel <sjkteel@hotmail.com>
2023-07-19 10:52:56 -0400

    placed all seeds into a single sql file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.urlencoded());
@@ -11,10 +11,13 @@ const db = mysql.createConnection(
         host: 'localhost',
         user: 'root',
         password: '224Ardmore',
-        database: 'buiness_db'
+        database: 'buisness_db'
     },
-    console.log(`Connected to the buiness_db database.`)
+    console.log(`Connected to the buisness_db database.`)
 );
 
+db.query('SELECT * FROM department', function (err, res) {
+    console.log(res);
+});
 
 app.listen(PORT, () => { console.log(`Working on ${PORT}`); });
